aab48a834337541c1643ade1f41984679e99f424
Arul Saxena <saxena@cs.ucsb.edu>
2022-09-06 13:16:22 -0700

    Add option to change macro goals through settings

--- makros/Views/SettingsView.swift
+++ makros/Views/SettingsView.swift
@@ -9,10 +9,39 @@ import SwiftUI
 
 struct SettingsView: View {
     
+    @AppStorage("calGoal") private var calGoal = 2000
+    @AppStorage("proteinGoal") private var proteinGoal = 120
+    @AppStorage("fatGoal") private var fatGoal = 70
+    @AppStorage("carbGoal") private var carbGoal = 240
+    
     var body: some View {
         VStack{
-            Text("blah")
-            Spacer()
+            // TODO: Sanitize inputs to positive integers
+            // Entering 0 for one of these values will cause issues
+            Form{
+                Section{
+                    HStack {
+                        Text("Calorie Goal: ")
+                        TextField("Calorie Goal",value: $calGoal, formatter: NumberFormatter())
+                            .keyboardType(.numberPad)
+                    }
+                    HStack {
+                        Text("Protein Goal: ")
+                        TextField("Protein Goal",value: $proteinGoal, formatter: NumberFormatter())
+                            .keyboardType(.numberPad)
+                    }
+                    HStack {
+                        Text("Fat Goal: ")
+                        TextField("Fat Goal",value: $fatGoal, formatter: NumberFormatter())
+                            .keyboardType(.numberPad)
+                    }
+                    HStack {
+                        Text("Carbohydrate Goal: ")
+                        TextField("Carbohydrate Goal",value: $carbGoal, formatter: NumberFormatter())
+                            .keyboardType(.numberPad)
+                    }
+                }
+            }
         }
         .navigationTitle("Settings")
     }
@@ -20,6 +49,8 @@ struct SettingsView: View {
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsView()
+        NavigationView{
+            SettingsView()
+        }
     }
 }
